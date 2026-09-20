a9bc9c9b630fd09d743b5a9045b7f9ce36b55db2
Zerlina Jacob <zerlinajv@gmail.com>
2026-09-20 13:30:41 +0530

    Updated builds

---
+++ Games/LittleTechExplorer1/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "ICT360-LittleTechExplorer-1.0";
+const contentToCache = [
+    "Build/Little Tech Explorers Game.loader.js",
+    "Build/Little Tech Explorers Game.framework.js.unityweb",
+    "Build/Little Tech Explorers Game.data.unityweb",
+    "Build/Little Tech Explorers Game.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
